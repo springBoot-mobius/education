0724b1a28cb3e9e18e570e9e51504a3656240fbe
caMelodian <intelliJHLee@gmail.com>
2022-11-28 15:53:30 +0900

    사용자 마이페이지 mapper test 1차

--- src/test/java/com/mobius/education/mapper/ApplyLectureMapperTest.java
+++ src/test/java/com/mobius/education/mapper/ApplyLectureMapperTest.java
@@ -1,6 +1,7 @@
 package com.mobius.education.mapper;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,7 +11,13 @@ public class ApplyLectureMapperTest {
     @Autowired
     private ApplyLectureMapper applyLectureMapper;
 
-//    public void selectTest(){
-//        log.info("내가 완료한 강좌 :" + ApplyLectureMapper.)
-//    }
+    @Test
+    public void selectTest(){
+        log.info("내가 완료한 강좌 :" + applyLectureMapper.selectEndLectureCount(0L));
+    }
+
+    @Test
+    public void selectTest01(){
+        log.info("진행 중인 강좌 :" + applyLectureMapper.selectOnGoingLectureCount(0L));
+    }
 }
